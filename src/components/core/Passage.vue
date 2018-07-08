99bea72298a8e40a3e857ba135f6b72772900d39
Dustin Woods <pyrozzcom@gmail.com>
2018-07-08 14:56:00 -0700

    Dash of emotion

--- src/components/core/Passage.vue
+++ src/components/core/Passage.vue
@@ -66,8 +66,19 @@ export default {
   .inline {
     display: inline;
   }
-  &.v-enter-active {
+  &.v-enter-active:not(.wake-up) {
     animation: fade-in 400ms;
+  }
+  &.v-enter-active.wake-up {
+    animation: shake-in 100ms;
+    &.slow {
+      animation-name: shake-in-less;
+      animation-duration: 600ms;
+    }
+    &.slowest {
+      animation-name: shake-in-less;
+      animation-duration: 800ms;
+    }
   }
   &.v-leave:not(.fallingAsleep) {
     display: none;
@@ -87,6 +98,85 @@ export default {
   100% {
     opacity: 1;
     transform: translate(0, 0px);
+  }
+}
+
+@keyframes shake-in {
+  0% {
+    opacity: 0;
+    transform: translateY(50px);
+    transform: translateX(110px);
+  }
+  10% {
+    transform: translateX(-100px);
+  }
+  20% {
+    transform: translateX(90px);
+  }
+  30% {
+    transform: translateX(-80px);
+  }
+  40% {
+    transform: translateX(70px);
+  }
+  50% {
+    transform: translateX(-60px);
+  }
+  60% {
+    transform: translateX(50px);
+  }
+  70% {
+    transform: translateX(-40px);
+  }
+  80% {
+    transform: translateX(30px);
+  }
+  90% {
+    transform: translateX(-20px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0) translateX(0);
+  }
+}
+
+
+@keyframes shake-in-less {
+  0% {
+    opacity: 0;
+    transform: translateY(50px);
+    transform: translateX(20px);
+  }
+  10% {
+    transform: translateX(-18px);
+  }
+  20% {
+    transform: translateX(16px);
+  }
+  30% {
+    transform: translateX(-14px);
+  }
+  40% {
+    transform: translateX(12px);
+  }
+  50% {
+    transform: translateX(-10px);
+  }
+  60% {
+    transform: translateX(8px);
+  }
+  70% {
+    transform: translateX(-6px);
+  }
+  80% {
+    transform: translateX(4px);
+  }
+  90% {
+    transform: translateX(-2px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0) translateX(0);
   }
 }
 
