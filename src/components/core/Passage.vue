d08f96fddfda2ca0e249e0ae0f9354c6a1532a70
Dustin Woods <pyrozzcom@gmail.com>
2018-07-08 13:54:50 -0700

    switched to better sleep transition

--- src/components/core/Passage.vue
+++ src/components/core/Passage.vue
@@ -1,6 +1,6 @@
 <template lang='pug'>
-  transition(v-on:enter='handleEnter' v-on:leave='handleLeave')
-    div(v-if='isCurrentPassage' class='passageContainer' v-bind:class='{inline}')
+  transition(@enter='handleEnter' @after-leave='handleLeave')
+    div(v-if='(isCurrentPassage && !fallingAsleep)' class='passageContainer' v-bind:class='{inline, fallingAsleep:(sleepAfter > 0)}')
       slot
 </template>
 
@@ -22,11 +22,26 @@ export default {
     inline: {
       default: false,
       type: Boolean,
+    },
+    sleepAfter: {
+      default: null,
+      type: Number,
+    }
+  },
+  data: function() {
+    return {
+      fallingAsleep: false,
     }
   },
   methods: {
+    startSleep() {
+      this.fallingAsleep = true;
+    },
     handleEnter() {
       this.$emit('enter');
+      if(this.sleepAfter !== null && this.sleepAfter > 0) {
+        setTimeout(this.startSleep, this.sleepAfter);
+      }
     },
     handleLeave() {
       this.$emit('leave');
@@ -44,6 +59,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+@import '../../scss/variables.scss';
+
 .passageContainer {
   .inline {
     display: inline;
@@ -51,9 +69,15 @@ export default {
   &.v-enter-active {
     animation: fade-in 400ms;
   }
-  &.v-leave {
+  &.v-leave:not(.fallingAsleep) {
     display: none;
   }
+  &.fallingAsleep.v-leave-active {
+    animation: fade-to-blur $sleep-transition-time;
+    animation-delay: $sleep-transition-delay;
+    animation-timing-function: cubic-bezier(1.000, 0.005, 0.470, 0.780);
+    animation-fill-mode: forwards;
+  }  
 }
 @keyframes fade-in {
   0% {
@@ -65,4 +89,24 @@ export default {
     transform: translate(0, 0px);
   }
 }
+
+@keyframes fade-to-blur {
+  0% {
+    opacity: 1;
+    filter: blur(0);
+    transform: scaleX(1) scaleY(1);
+  }
+  75% {
+    transform: scaleX(1) scaleY(1);
+  }
+  80% {
+    opacity: 0;
+    filter: blur(4px);
+    transform: scaleX(1.8) scaleY(1.5);
+  }
+  100% {
+    opacity: 0;
+  }
+}
+
 </style>
